47326f82a7e072e67d8d5abe93cf6ed9cd328ddf
zhao-xxxx <17302550892@qq.com>
2022-04-13 09:38:17 +0800

    '鑷姩鎻愪氦'

--- auto deploy.bat
+++ auto deploy.bat
@@ -1,4 +1,6 @@
 @echo off
-D:
-cd D:\MyCodingRoom\MyBlog
-hexo clean&&hexo g&&hexo d 
+E:
+cd E:\1学习\在校工作\2工作准备\0push
+git add .
+git commit -m '自动提交'
+git push -u git@github.com:zhao-xxxx/learning-note.git
